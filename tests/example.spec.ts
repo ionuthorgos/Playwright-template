00e3a605fb376678ee2c5486390a8968c8406fee
horgosionut <horgosdumitru@gmail.com>
2024-03-13 19:15:43 +0200

    fix the comment

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,7 +16,7 @@ test.describe('Tests with isolated browser contexts', () => {
       `What's Installed`,
       `How to run the example test`,
       `How to open the HTML test report`,
-      `Write tests using web first assertions, page fixtures aaaaand locators`,
+      `Write tests using web first assertions, page fixtures and locators`,
       `Run single test, multiple tests, headed mode`,
       `Generate tests with Codegen`,
       `See a trace of your tests`
