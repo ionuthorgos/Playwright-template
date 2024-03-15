40a8de934595d9ffd686bd8ac0ab1aa8eecfb3df
horgosionut <horgosdumitru@gmail.com>
2024-03-15 12:57:36 +0200

    add comments for careers test

--- tests/careers.spec.ts
+++ tests/careers.spec.ts
@@ -8,12 +8,18 @@ test.describe('Tests with isolated browser contexts', () => {
     }
   });
   
-  test('Search for a career', async ({ mainPage, helpersUi }) => {
+  test('Searching career opportunities', async ({ mainPage, helpersUi }) => {
+    // Navigate to the base URL defined in the config file
     await helpersUi.goto();
+    // Interact with the page to accept all cookies, ensuring compliance with privacy regulations
     await helpersUi.selectElement(mainPage.acceptAllCookies)
+    // Hover over the careers button to reveal any potential sub-menus.
     await mainPage.hoverOver(mainPage.careersButton)
+    // Verify that the sub-menu related to careers is visible after the hover action.
     await mainPage.checkSubMenuVisibility()
+    // Assert that the sub-menu title contains the expected text "Work with us".
     await helpersUi.elementContainText(mainPage.subMenuTitleFromCareers,"Work with us")
+    // Select the button to search for careers.
     await helpersUi.selectElement(mainPage.searchCareersButton)
   });
 
