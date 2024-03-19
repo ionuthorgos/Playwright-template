5c7bdb91933bfa7b3f695bef50866a36f83c454d
horgosionut <horgosdumitru@gmail.com>
2024-03-19 19:47:21 +0200

    add schema for create user

--- tests/exampleApi.spec.ts
+++ tests/exampleApi.spec.ts
@@ -10,24 +10,36 @@ test.describe('API tests', () => {
   });
 
   test('GET request to retrieve posts', async () => {
-    const response = await apiHelpers.makeRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+    const response = await apiHelpers.makeRequest('GET', 'http://localhost:3000/users');
     expect(response.status()).toBe(200);
     const posts = await response.json();
+    console.log({posts})
+    expect(posts[0].name).toContain('User One')
     expect(posts).not.toHaveLength(0);
   });
+  
+  test('Create a new user', async () => {
+    const newUserResponse = await apiHelpers.makeRequest('POST', 'http://localhost:3000/users', 
+        { name: 'New User', email: 'newuser@example.com' }
+      );
+      expect(newUserResponse.status()).toBe(201);
+      const posts = await newUserResponse.json();
 
-  test('POST request to create a post', async () => {
-    const postData = {
-      title: 'foo',
-      body: 'bar',
-      userId: 1,
-    };
-    const response = await apiHelpers.makeRequest('POST', 'https://jsonplaceholder.typicode.com/posts', postData);
-    expect(response.status()).toBe(201);
-    const post = await response.json();
-    expect(post).toHaveProperty('id');
-    expect(post.title).toBe(postData.title);
-    expect(post.body).toBe(postData.body);
-    expect(post.userId).toBe(postData.userId);
-  });
+      console.log(JSON.stringify(posts))
+  })
+
+  // test('POST request to create a post', async () => {
+  //   const postData = {
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 1,
+  //   };
+  //   const response = await apiHelpers.makeRequest('POST', 'https://jsonplaceholder.typicode.com/posts', postData);
+  //   expect(response.status()).toBe(201);
+  //   const post = await response.json();
+  //   expect(post).toHaveProperty('id');
+  //   expect(post.title).toBe(postData.title);
+  //   expect(post.body).toBe(postData.body);
+  //   expect(post.userId).toBe(postData.userId);
+  // });
 });
